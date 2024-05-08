9427bb0fb1fb1e377cfebea675a060620f04184b
nejdbedoui <74482153+nejdbedoui@users.noreply.github.com>
2024-05-08 01:26:18 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   .build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
-await app.listen(3001);
+await app.listen(process.env.PORT,'0.0.0.0');
 
 }
 bootstrap();
